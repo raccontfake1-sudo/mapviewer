e84d70597e045b7ff6f018767945f1ad968f128c
raccontfake1-sudo <raccontfake1@gmail.com>
2026-06-07 21:57:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ import html
 import re
 import time
 
-for pct, msg in [
-    (10, "Loading ECC Control"),
-    (25, "Loading NIST Controls"),
-    (40, "Extracting Metadata"),
-    (55, "Generating Semantic Embeddings"),
-    (70, "Applying Ontology Scoring"),
-    (85, "Generating AI Explanation"),
-    (100, "Complete")
 ]:
     progress_bar.progress(pct)
     status_text.write(msg)
